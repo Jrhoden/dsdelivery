fb3389fafa3d61c54dc8cde79333ce40937ce8eb
Jeferson Fetter Rhoden <jeferson.rhoden@cwi.com.br>
2021-01-09 16:22:31 -0300

    Create front web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import './App.css';
+function App() {
+
+  useEffect(() => {
+    //chamada para API para buscar produtos
+  }, [])
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
